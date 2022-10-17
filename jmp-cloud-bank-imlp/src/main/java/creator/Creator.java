fec6a820c3f55ceca825aaf4ec7643879cbce9b2
Maxim <maximgrigorovua@gmail.com>
2022-10-17 10:30:37 +0300

    Impl

--- jmp-cloud-bank-imlp/src/main/java/creator/Creator.java
+++ jmp-cloud-bank-imlp/src/main/java/creator/Creator.java
@@ -5,11 +5,10 @@ import model.BankCardType;
 import model.User;
 import repo.Bank;
 
-import java.time.LocalDate;
-
 public class Creator implements Bank {
     @Override
     public BankCard createBankCard(User user, BankCardType bankCardType) {
-        return new BankCard("123123", new User(" ", " ", LocalDate.now()));
+
+        return new BankCard(bankCardType.name(), user);
     }
 }
